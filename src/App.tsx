fa5e4ffd6421a38b863ebc1a36f4bace8696fb6e
Kent Claudio <rikysmile33@gmail.com>
2021-09-22 02:15:31 -0700

    Refactored project structure
    Created css folder to store CSS files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
       <p>This is app.</p>
+      <footer>Web app created by @Exarch-Eden</footer>
     </div>
   );
 }
