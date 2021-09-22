a40f169f5ca8142d897db0dbeea3972f52023f65
Kent Claudio <rikysmile33@gmail.com>
2021-09-22 02:59:41 -0700

    Updated App TSX file and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './css/App.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Routes from "./components/Routes";
+
+import "./css/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>This is app.</p>
-      <footer>Web app created by @Exarch-Eden</footer>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <header>
+                  <NavBar />
+                </header>
+                <main>
+                    <Routes />
+                    {/* <p className="LightText">This is app.</p> */}
+                </main>
+                <footer className="Padded">
+                    Web app created by @Exarch-Eden
+                </footer>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
