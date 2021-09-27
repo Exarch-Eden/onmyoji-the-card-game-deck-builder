7825ff87ff01f5aaed84d886f1526603dc15d64a
Exarch-Eden <rikysmile33@gmail.com>
2021-09-27 10:11:11 -0700

    Implemented Card component to DeckBuilder screen for testing

--- src/screens/DeckBuilder.tsx
+++ src/screens/DeckBuilder.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import Card from '../components/Card'
 
 import '../css/DeckBuilder.css'
+import { CardInfo } from '../types'
+
+const CardInfoTest: CardInfo = {
+    name: 'Akaname',
+    desc: 'Description',
+    type: 'Spell',
+    imageSrc: './images/akaname/2.jpg'
+}
 
 const DeckBuilder = () => {
   return (
     <div className="DeckBuilderContainer">
         <div className="DeckBuilderLeft"><p>Left</p></div>
-        <div className="DeckBuilderRight"><p>Right</p></div>
+        <div className="DeckBuilderRight">
+            <p>Right</p>
+            <Card {...CardInfoTest} />
+        </div>
       {/* <p>This is Deck Builder</p> */}
     </div>
   )
