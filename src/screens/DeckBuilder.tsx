67d79c3981fb42234d498e7a9be3ac1afc9c6243
Exarch-Eden <rikysmile33@gmail.com>
2021-09-30 10:21:23 -0700

    Refactored DeckBuilder
    Auto-formatted using Prettier

--- src/screens/DeckBuilder.tsx
+++ src/screens/DeckBuilder.tsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import Card from '../components/Card'
+import React from 'react';
+import Card from '../components/Card';
 
-import '../css/DeckBuilder.css'
-import { CardInfo } from '../types'
+import '../css/DeckBuilder.css';
+import { CardInfo } from '../types';
 
 const CardInfoTest: CardInfo = {
     name: 'Akaname',
     desc: 'Description',
     type: 'Spell',
     imageSrc: './images/akaname/2.jpg'
-}
+};
 
 const DeckBuilder = () => {
-  return (
-    <div className="DeckBuilderContainer">
-        <div className="DeckBuilderLeft"><p>Left</p></div>
-        <div className="DeckBuilderRight">
-            <p>Right</p>
-            <Card {...CardInfoTest} />
+    return (
+        <div className='DeckBuilderContainer'>
+            <div className='DeckBuilderLeft'>
+                <p>Left</p>
+            </div>
+            <div className='DeckBuilderRight'>
+                <p>Right</p>
+                <Card {...CardInfoTest} />
+            </div>
         </div>
-      {/* <p>This is Deck Builder</p> */}
-    </div>
-  )
-}
+    );
+};
 
-export default DeckBuilder
+export default DeckBuilder;
