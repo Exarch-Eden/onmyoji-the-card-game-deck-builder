2cb6f2fd17a2652260256e5a22a353270338fcb2
Kent Claudio <rikysmile33@gmail.com>
2021-10-03 01:33:51 -0700

    Updated and refactored Card component
    Implemented card type description container PNG files
    
    Created function to render proper card description container based on card type
    
    Added several child components to store card information

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,10 +3,56 @@ import { CardInfo, CardType } from "../types";
 
 import "../css/Card.css";
 
-const reducer = (state = "ShikigamiCard", action: { type: CardType }) => {
-    console.log("action.type: ", action.type);
+import ASSETS from "../assets/assets";
 
-    let cardType = state;
+const Card: FC<CardInfo> = ({ name, shikigamiName, desc, imageSrc, type }) => {
+    const [cardType, cardDispatch] = useReducer(cardReducer, "ShikigamiCard");
+
+    useEffect(() => {
+        console.log("type: ", type);
+
+        cardDispatch({ type: type });
+    }, [type]);
+
+    return (
+        <div className={`CardBackground ${cardType}`}>
+            <div className={`Card ${cardType}`}>
+                {type === "Field" ? <div className="FieldCardTop"></div> : null}
+                <img
+                    className="CardImage"
+                    src={imageSrc}
+                    alt={`${name} Card`}
+                />
+                {renderDescContainer(type, desc)}
+                <div className="CardBottom">
+                    <p>
+                        {shikigamiName} - {type === "Evolve" ? "Spell" : type}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const renderDescContainer = (type: CardType, desc: string) => {
+    const cName = "CardDescription";
+    const alt = "Description Container";
+
+    if (type === "Shikigami" || type === "Form") {
+        return <img className={cName} src={ASSETS.formDesc} alt={alt}></img>;
+    } else if (type === "Combat") {
+        return <img className={cName} src={ASSETS.combatDesc} alt={alt}></img>;
+    } else if (type === "Field") {
+        return <img className={cName} src={ASSETS.fieldDesc} alt={alt}></img>;
+    } else {
+        // Evolve or Spell types
+        return <img className={cName} src={ASSETS.spellDesc} alt={alt}></img>;
+    }
+};
+
+const cardReducer = (state: string, action: { type: CardType }): string => {
+    let cardType = "ShikigamiCard";
+
     switch (action.type) {
         case "Combat":
             cardType = "CombatCard";
@@ -26,28 +72,6 @@ const reducer = (state = "ShikigamiCard", action: { type: CardType }) => {
     }
 
     return cardType;
-};
-
-const Card: FC<CardInfo> = ({ name, desc, imageSrc, type }) => {
-    const [cardType, dispatch] = useReducer(reducer, "ShikigamiCard");
-
-    useEffect(() => {
-        console.log("type: ", type);
-
-        dispatch({ type: type });
-    }, [type]);
-
-    return (
-        <div className={`CardBackground ${cardType}`}>
-            <div className={`Card ${cardType}`}>
-                <img
-                    className="CardImage"
-                    src={imageSrc}
-                    alt={`${name} Card`}
-                />
-            </div>
-        </div>
-    );
 };
 
 export default Card;
