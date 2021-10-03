f5085123b84b0021e89d2a2545098d7c21930286
Kent Claudio <rikysmile33@gmail.com>
2021-10-03 01:29:31 -0700

    Added PNG files for card type description containers

---
+++ src/assets/assets.js
@@ -0,0 +1,13 @@
+import spellDesc from "./spell-desc.png";
+import combatDesc from "./combat-desc.png";
+import fieldDesc from "./field-desc.png";
+import formDesc from "./form-desc.png";
+
+const defaultExport = {
+  spellDesc,
+  combatDesc,
+  fieldDesc,
+  formDesc
+}
+
+export default defaultExport;
