745f7708046db10eb7fa8a374d9dc451ece4a40d
Exarch-Eden <rikysmile33@gmail.com>
2021-10-04 10:02:07 -0700

    Updated downloader JS file
    Refactored IMAGE_NUMS array

--- downloader.js
+++ downloader.js
@@ -6,6 +6,99 @@ const SIZE_SUFFIX = "_640X960.jpg";
 
 const DESTINATION_DIRECTORY = "./public/images/";
 
+const IMAGE_NUMS = [
+    {
+        expansion: "good-or-evil",
+        cards: [
+            {
+                name: "asura",
+                startCardNum: 223,
+                endCardNum: 231,
+            },
+            {
+                name: "orochi",
+                startCardNum: 232,
+                endCardNum: 241,
+            },
+            {
+                name: "taishakuten",
+                startCardNum: 242,
+                endCardNum: 250,
+            },
+            {
+                name: "akaname",
+                startCardNum: 251,
+                endCardNum: 259,
+            },
+            {
+                name: "odokuro",
+                startCardNum: 260,
+                endCardNum: 268,
+            },
+            {
+                name: "kingyo",
+                startCardNum: 269,
+                endCardNum: 277,
+            },
+            {
+                name: "kanihime",
+                startCardNum: 278,
+                endCardNum: 286,
+            },
+            {
+                name: "tamamonomae",
+                startCardNum: 287,
+                endCardNum: 295,
+            },
+        ]
+    },
+    {
+        expansion: "four-guardians",
+        cards: [
+            {
+                name: "hitotsume",
+                startCardNum: 296,
+                endCardNum: 304,
+            },
+            {
+                name: "gaki",
+                startCardNum: 305,
+                endCardNum: 315,
+            },
+            {
+                name: "joruri-gozen",
+                startCardNum: 316,
+                endCardNum: 324,
+            },
+            {
+                name: "kyuumi-neko",
+                startCardNum: 325,
+                endCardNum: 334,
+            },
+            {
+                name: "hitorima",
+                startCardNum: 335,
+                endCardNum: 343,
+            },
+            {
+                name: "menreiki",
+                startCardNum: 344,
+                endCardNum: 356,
+            },
+            {
+                name: "sayuki-hime",
+                startCardNum: 357,
+                endCardNum: 366,
+            },
+            {
+                name: "genkuro",
+                startCardNum: 367,
+                endCardNum: 375,
+            },
+        ]
+    }
+];
+
 const download = (url, dest, cb) => {
     // check if file exists already
     fs.stat(dest, (err, stat) => {
@@ -33,56 +126,18 @@ const download = (url, dest, cb) => {
     });
 };
 
-const IMAGE_NUMS = [
-    {
-        name: "asura",
-        startCardNum: 223,
-        endCardNum: 231,
-    },
-    {
-        name: "orochi",
-        startCardNum: 232,
-        endCardNum: 241,
-    },
-    {
-        name: "taishakuten",
-        startCardNum: 242,
-        endCardNum: 250,
-    },
-    {
-        name: "akaname",
-        startCardNum: 251,
-        endCardNum: 259,
-    },
-    {
-        name: "odokuro",
-        startCardNum: 260,
-        endCardNum: 268,
-    },
-    {
-        name: "kingyo",
-        startCardNum: 269,
-        endCardNum: 277,
-    },
-    {
-        name: "kanihime",
-        startCardNum: 278,
-        endCardNum: 286,
-    },
-    {
-        name: "tamamonomae",
-        startCardNum: 287,
-        endCardNum: 295,
-    },
-];
+const EXPANSION_INDEX = 1;
 
+const curCardList = IMAGE_NUMS[EXPANSION_INDEX].cards;
+const curExpansion = IMAGE_NUMS[EXPANSION_INDEX].expansion;
 // change curShikigami variable value based on which shikigami to fetch images for
-for (let curShikigami = 3; curShikigami < IMAGE_NUMS.length; curShikigami++) {
-    const startNum = IMAGE_NUMS[curShikigami].startCardNum;
-    const endNum = IMAGE_NUMS[curShikigami].endCardNum;
-    const directorySuffix = IMAGE_NUMS[curShikigami].name;
+for (let curShikigami = 0; curShikigami < curCardList.length; curShikigami++) {
+    const startNum = curCardList[curShikigami].startCardNum;
+    const endNum = curCardList[curShikigami].endCardNum;
+    const directorySuffix = `${curExpansion}/${curCardList[curShikigami].name}`;
 
     const fullPath = `${DESTINATION_DIRECTORY}${directorySuffix}`;
+    console.log('fullPath: ', fullPath);
 
     fs.stat(fullPath, (err, stat) => {
         if (err && err.code === "ENOENT") {
