045a7eeacdd0b87f9dc10c3c70f7736a0942d937
Kent Claudio <rikysmile33@gmail.com>
2021-10-26 22:33:11 -0700

    Updated types TS file
    Added StatType for different stat types (health, shield, attack, etc.)

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,18 @@
 export type CardType = "Shikigami" | "Combat" | "Form" | "Spell" | "Field" | "Evolve"
 
+export type StatType = "Health" | "Shield" | "Attack" | "Intensity" | "Fragile"
+
+export type StatInfo = {
+    statType: StatType;
+    statValue: number;
+}
+
 export interface CardInfo {
     name: string;
     shikigamiName: string;
     desc: string;
     imageSrc: string;
     type: CardType;
+    leftStat?: StatInfo;
+    rightStat?: StatInfo;
 }
