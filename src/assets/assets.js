4bef48892a30977abd744560ebbe7b1456ddb518
Kent Claudio <rikysmile33@gmail.com>
2021-10-26 22:31:24 -0700

    Added stat icons

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,11 +3,22 @@ import combatDesc from "./combat-desc.png";
 import fieldDesc from "./field-desc.png";
 import formDesc from "./form-desc.png";
 
+import attackIcon from "./stat-icons/attack-icon.png"
+import intensityIcon from "./stat-icons/intensity-icon.png"
+import fragileIcon from "./stat-icons/fragile-icon.png"
+import healthIcon from "./stat-icons/health-icon.png"
+import shieldIcon from "./stat-icons/shield-icon.png"
+
 const defaultExport = {
   spellDesc,
   combatDesc,
   fieldDesc,
-  formDesc
+  formDesc,
+  attackIcon,
+  intensityIcon,
+  fragileIcon,
+  healthIcon,
+  shieldIcon
 }
 
 export default defaultExport;
